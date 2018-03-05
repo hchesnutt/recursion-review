d3b255a63eb1db93b9f05b278c3d7360e515cda3
hchesnutt <t.henry.chesnutt@gmail.com>
2018-03-05 15:05:09 -0800

    Revert "pass all tests for stringifyJSON"
    This reverts commit 5dad633ff77f7048208f85e766d8f4f5e51df46b.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,37 +4,5 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // declare stringifiedJSON
-  var stringifiedJSON = '';
-  // Invoke evaluate on each key in obj
-  for (var key of obj) {
-    
-  }
-  // concat to stringifiedJSON
-};
-
-
-var evaluate = function(val) {
-  switch (typeof val) {
-  case 'undefined':
-    return 'undefined';
-    break;
-  case 'boolean':
-    return val ? 'true' : 'false';
-    break;
-  case 'number':
-    return val.toString();
-    break;
-  case 'string':
-    return `${val}`;
-    break;
-  default:
-    if (Array.isArray(val)) {
-      for (var i = 0; i < val.length - 1; i++) {
-        return stringifyJSON(val[i]);
-      }
-    } else {
-      return stringifyJSON(val);
-    }
-  } 
+  // your code goes here
 };
