f76d9169503fafe2b9bca66d29b1b50888923531
hchesnutt <t.henry.chesnutt@gmail.com>
2018-03-05 15:04:32 -0800

    pass all tests for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,37 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // declare stringifiedJSON
+  var stringifiedJSON = '';
+  // Invoke evaluate on each key in obj
+  for (var key of obj) {
+    
+  }
+  // concat to stringifiedJSON
+};
+
+
+var evaluate = function(val) {
+  switch (typeof val) {
+  case 'undefined':
+    return 'undefined';
+    break;
+  case 'boolean':
+    return val ? 'true' : 'false';
+    break;
+  case 'number':
+    return val.toString();
+    break;
+  case 'string':
+    return `${val}`;
+    break;
+  default:
+    if (Array.isArray(val)) {
+      for (var i = 0; i < val.length - 1; i++) {
+        return stringifyJSON(val[i]);
+      }
+    } else {
+      return stringifyJSON(val);
+    }
+  } 
 };
