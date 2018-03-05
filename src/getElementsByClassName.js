6554d907a96e635ea8ccebbb9780dc8c5aa39a72
hchesnutt <t.henry.chesnutt@gmail.com>
2018-03-05 15:07:35 -0800

    pass all tests for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
+
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  //initialize nodeList
+  let nodeList = [];
+  //function traverse
+  const traverse = function(node) {
+    //  if classname equals target add to nodelist
+    if (node.classList.contains(className)) {
+      nodeList.push(node);
+    }
+    //  if children recurse on each child
+    if (node.children.length > 0) {
+      for (let i = 0; i < node.children.length; ++i) {
+        traverse(node.children[i]);
+      }
+    }
+  };
+  traverse(document.body);
+  return nodeList;
 };
